629083e7f4381d12b0ade380b42cc06c79206d39
timmy1691 <67081825+timmy1691@users.noreply.github.com>
2021-01-19 23:10:44 +0000

    continue question 2, checking device stats

--- question2.py
+++ question2.py
@@ -1,8 +1,24 @@
 # beginning of file
 # import packages
 import timeit
+import os
+import platform
 
 start = timeit.default_timer()
 
-#begin with the body of the cody
+#begin with the body of the code
 
+# hostname
+print("Device Name:", platform.node())
+
+#operating system name
+print("Operating System Provider and Version", platform.system(), platform.release())
+
+#cpu count
+print("Number of CPUs:", os.cpu_count())
+
+# getting ram
+print(os.sys)
+
+end = timeit.default_timer()
+print(end - start)
